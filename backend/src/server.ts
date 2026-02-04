cba68b28341b80530fb0beb47df701fc44de287c
MacBook Air <macbookair@MacBooks-MacBook-Air-2.local>
2026-02-04 19:21:49 +0800

    Initial Commit

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log("Running on port " + PORT);
+})
